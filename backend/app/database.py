e20e9d1b14e2d1f90d4a0112289491741571e637
LauraSangalli <lauraasangalli@gmail.com>
2026-07-25 22:13:32 -0300

    feat: create database.py

---
+++ backend/app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///jobs.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+session_local = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
